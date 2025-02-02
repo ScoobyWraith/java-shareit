cd5195edf90b791e81be4de421a6281a1469bc1b
m.kiselev <sanwiraight@gmail.com>
2025-02-02 21:30:25 +0300

    [Sprint 14] Optimize imports

--- src/main/java/ru/practicum/shareit/item/Item.java
+++ src/main/java/ru/practicum/shareit/item/Item.java
@@ -1,7 +1,5 @@
 package ru.practicum.shareit.item;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
